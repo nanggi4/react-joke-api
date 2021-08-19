8b0232842379fd673562fa773cdbd1dff565c6fd
nanggi4 <nkhun1006@gmail.com>
2021-08-19 14:33:33 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import clsx from 'clsx';
+import { withStyles } from '@material-ui/core/styles';
+import { Typography, Paper, Button, Container } from '@material-ui/core';
 
-function App() {
+const App = (props) => {
+  const classes = props.classes;
+  const [joke, setJoke] = useState("");
+
+  const API_URL = "http://api.icndb.com/jokes/random";
+
+  const getJoke = () => {
+    axios.get(API_URL)
+    .then(res => {
+      const randomJoke = res.data.value["joke"];
+      console.log(typeof randomJoke);
+      setJoke(randomJoke);
+    })
+    .catch(err => console.error(err));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="sm">
+        <Typography variant="h4" gutterBottom className={clsx(classes.title)} align="center">
+          React Random Joke Generator 🤣
+        </Typography>      
+        <Paper elevation={3} className={clsx(classes.jokeContainer)}>
+          {joke && (
+            <Typography variant="h6" gutterBottom className={clsx(classes.joke)} align="center">
+              {joke}
+            </Typography>
+          )}
+          <Button 
+            variant="contained" 
+            size="large" 
+            color="primary" 
+            className={clsx(classes.jokeBtn)} 
+            fullWidth
+            onClick={getJoke}
+          >
+              Get Joke 😁
+          </Button>
+        </Paper>
+      </Container>
     </div>
   );
 }
 
-export default App;
+const styles = {
+  title: {
+    color: 'white',
+    fontFamily: 'Inconsolata',
+    marginBottom: '2rem'
+  },
+  jokeContainer: {
+    padding: '1rem',
+    textAlign: 'center'
+  },
+  joke: {
+    marginBottom: '0.5rem'
+  }
+};
+
+export default withStyles(styles)(App);
